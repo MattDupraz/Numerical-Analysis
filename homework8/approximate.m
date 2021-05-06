d133b7e8aa53f6797a411d31d1da13faafdcf540
MattDupraz <matthew.dupraz@gmail.com>
2021-05-06 16:30:26 +0200

    Finished HW8

--- homework8/approximate.m
+++ homework8/approximate.m
@@ -1,8 +1,10 @@
 function [y]=approximate(f, N, z, lambda)
-	k = @(x, y) exp(-(x-y).^2/2);
-	x = (0:N)/N;
-	K = k(x', x);
+    k = @(x, y) exp(-(x-y).^2/2);
+    x = (0:N)/N;
+    K = k(x', x);
+    % regularize matrix K
     K = K + lambda*eye(size(K));
-	Z = k(z', x);
-	y = Z*(K\f(x'));
+    Z = k(z', x);
+    disp(cond(K, inf));
+    y = Z*(K\f(x'));
 
--- homework8/main.m
+++ homework8/main.m
@@ -1,9 +1,13 @@
+% We define the functions we use
 f1 = @(x) exp(x);
 f2 = @(x) sqrt(abs(1/2 - x));
 
+% Functions and values of N we want to
+% iterate over
 fs = {f1, f2};
 Ns = [2, 5, 10];
 
+% Evaluation points
 z = linspace(0, 1, 1000);
 
 errors = zeros(2, 3);
@@ -13,9 +17,11 @@ for i=1:2
     y = f(z);
     for j=1:3
         N = Ns(j);
+        % Calculate approximated values
         approx_y = approximate(f, N, z, 0)';
         errors(i, j) = max(abs(approx_y - y));
-        
+
+        % Display results
         subplot(3, 2, i + 2*(j-1));
         plot(z, approx_y);
         hold on;
